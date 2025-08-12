2f4f44e81dda1e303c5b0bf636e5fdb4ee0c8fd8
Nikola <nesto@npr.com>
2025-08-12 16:22:59 +0200

    dodao react projekat sa stabilnom eslint i prettier konfiguracijom, plus dodatno jedan dio formi za admina

---
+++ novi_projekat/src/App.js
@@ -0,0 +1,11 @@
+import RegistrationRequests from "./pages/administrator/RegistrationRequests";
+
+function App() {
+  return (
+    <div className="App">
+      <RegistrationRequests />
+    </div>
+  );
+}
+
+export default App;
